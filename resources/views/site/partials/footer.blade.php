d1606f717a5981abcb6f2801e98db7589ff1445b
alfjuniorbh <alfjuniorbh.web@gmail.com>
2019-02-08 10:25:28 -0200

    Update email

--- resources/views/site/partials/footer.blade.php
+++ resources/views/site/partials/footer.blade.php
@@ -8,7 +8,7 @@
                         <p>
                                 Av. Olinto Meireles, Nº 65 - Barreiro de Baixo | CEP: 30.640-010 - Belo Horizonte/MG - Brasil 
                                 Telefone: <a href="tel:553133282817">+55 (31) 3328-2817</a> | 
-                                E-mail: <a href="mailto:contato@vallourec.com" target="_blank">contato@vallourec.com</a>
+                                E-mail: <a href="mailto:eixoferroviariotubular@vallourec.com" target="_blank">eixoferroviariotubular@vallourec.com</a>
                         </p>
                 </div>
         </div>
